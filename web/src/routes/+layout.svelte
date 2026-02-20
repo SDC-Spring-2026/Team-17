678aeee873f704fa5b73f289c560f976a8c26c7d
Ethan Meng <uwisc@endfind.me>
2026-02-20 05:35:14 -0600

    the URL changed
    oh my god why

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -17,7 +17,7 @@
   <p class="text-center text-sm italic text-gray-500 px-6 m-auto w-full">
     This site is run by <a href="https://rapidcow.github.io/site-wisc-sdc-fa25-team/"
       >SDC Fall 2025 Team 17</a
-    >! Read our source code <a href="https://github.com/SDC-Fall-2025/Team-17-Club-Radar">here</a>.
+    >! Read our source code <a href="https://github.com/SDC-Fall-2025/Team-17">here</a>.
   </p>
   <p class="text-center"></p>
   <p class="text-center text-gray-500 px-6 m-auto w-2xl">
